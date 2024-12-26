0928b695cbffc49bfb41ff507f815a77c18af986
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-12-26 17:40:18 -0500

    Reset optimization parameters

--- optimize/parameters.py
+++ optimize/parameters.py
@@ -2,13 +2,14 @@ import numpy as np
 
 
 parameters = {
-    'squad_evaluation_round_factor': 0.7915879292906937,
-    'captain_multiplier': 2,
-    'starting_xi_multiplier': 1,
-    'reserve_gkp_multiplier': 0.26780126530565046,
-    'reserve_out_multiplier': 0.3749016858132089 ** np.arange(1, 4),
-    'future_gameweeks_evaluated': 4,
-    'budget_importance': 3.490974459751069e-07,
+    'squad_evaluation_round_factor': 1.0,
+    'captain_multiplier': 2.0,
+    'starting_xi_multiplier': 1.0,
+    'vice_captain_multiplier': 1.0,
+    'reserve_gkp_multiplier': 0.0,
+    'reserve_out_multiplier': [0.0, 0.0, 0.0],
+    'future_gameweeks_evaluated': 1,
+    'budget_importance': 0,
     'transfer_aversion_factor': 1.0,
 }
 
