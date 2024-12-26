277bc138d8784a9c5d6fe9875462c7d6f0a7e720
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-12-26 17:41:38 -0500

    Hook up the MILP solver

--- optimize/__init__.py
+++ optimize/__init__.py
@@ -1,22 +1,109 @@
+from collections import defaultdict
+
 import pandas as pd
 
-from optimize.utilities import get_future_gameweeks, make_best_transfer
 from optimize.greedy import run_greedy_optimization
+from optimize.utilities import suggest_squad_roles
+from optimize.utilities import get_future_gameweeks
+from optimize.cp import solve
+from optimize.parameters import get_parameter
 
 
 def optimize_squad(
-    current_season: str, current_squad: set, current_budget: int, 
-    next_gameweek: int, wildcard_gameweeks: list,
-    now_costs: pd.Series, selling_prices: pd.Series, 
-    gameweek_elements: pd.DataFrame, gameweek_predictions: pd.Series,
-    free_transfers: int, transfers_made: int
+    current_season: str, 
+    current_squad: set, 
+    current_budget: int, 
+    free_transfers: int,
+    next_gameweek: int, 
+    wildcard_gameweeks: list,
+    predictions: pd.Series,
+    elements: pd.DataFrame, 
+    now_costs: pd.Series, 
+    selling_prices: pd.Series, 
 ):
-    """Make transfers to optimize the squad for the next gameweek."""
+
+    return cp_optimization(
+        current_season, current_squad, current_budget, free_transfers,
+        next_gameweek, wildcard_gameweeks, predictions, elements, now_costs, selling_prices
+    )
+
+
+def cp_optimization(
+    current_season: str, 
+    current_squad: set, 
+    current_budget: int, 
+    free_transfers: int,
+    next_gameweek: int, 
+    wildcard_gameweeks: list,
+    predictions: pd.Series,
+    elements: pd.DataFrame, 
+    now_costs: pd.Series, 
+    selling_prices: pd.Series, 
+):
+    
+    gameweeks = get_future_gameweeks(next_gameweek, wildcard_gameweeks=wildcard_gameweeks)
+
+    if (current_season == '2022-23') and (7 in gameweeks):
+        gameweeks.remove(7)
+
+    # Prepare input for the solver
+    players = list(elements.index)
+    teams = elements['team'].to_dict()
+    element_types = elements['element_type'].to_dict()
+    points = defaultdict(lambda: 0, predictions.to_dict())
+    now_costs = now_costs.to_dict()
+    selling_prices = selling_prices.to_dict()
+
+    for p in players:
+        if p not in selling_prices:
+            selling_prices[p] = now_costs[p]
+
+    solution = solve(
+        gameweeks,
+        players,
+        teams,
+        element_types,
+        points,
+        current_squad,
+        current_budget,
+        free_transfers,
+        now_costs,
+        selling_prices,
+        wildcard_gameweeks,
+        get_parameter('squad_evaluation_round_factor'),
+        get_parameter('starting_xi_multiplier'),
+        get_parameter('captain_multiplier'),
+        get_parameter('vice_captain_multiplier'),
+        get_parameter('reserve_gkp_multiplier'),
+        get_parameter('reserve_out_multiplier')[0],
+        get_parameter('reserve_out_multiplier')[1],
+        get_parameter('reserve_out_multiplier')[2],
+    )
+
+    return solution[next_gameweek]
+
+
+def greedy_optimization(
+    current_season: str, 
+    current_squad: set, 
+    current_budget: int, 
+    free_transfers: int,
+    next_gameweek: int, 
+    wildcard_gameweeks: list,
+    gameweek_predictions: pd.Series,
+    gameweek_elements: pd.DataFrame, 
+    now_costs: pd.Series, 
+    selling_prices: pd.Series, 
+    transfers_made: int = 0
+):
 
     future_gameweeks = get_future_gameweeks(next_gameweek, wildcard_gameweeks=wildcard_gameweeks)
 
     if (current_season == '2022-23') and (7 in future_gameweeks):
         future_gameweeks.remove(7)
+
+    if next_gameweek in {1, *wildcard_gameweeks}:
+        free_transfers = float('inf')
 
     best_squad = run_greedy_optimization(
         current_squad, current_budget, future_gameweeks, 
@@ -24,4 +111,15 @@ def optimize_squad(
         free_transfers, transfers_made
     )
 
-    return best_squad
+    best_roles = suggest_squad_roles(
+        best_squad, 
+        gameweek_elements['element_type'], 
+        gameweek_predictions.loc[:, next_gameweek]
+    )
+
+    solution = {
+        'squad': best_squad,
+        **best_roles
+    }
+
+    return solution
--- simulation/__init__.py
+++ simulation/__init__.py
@@ -1,33 +1,34 @@
 import pandas as pd
-import numpy as np
 
 from optimize import optimize_squad
-from optimize.utilities import suggest_squad_roles, calculate_points, calculate_budget, sum_player_points, update_purchase_prices, update_selling_prices, calculate_transfer_cost, count_transfers_made
+from optimize.utilities import calculate_points, calculate_budget, sum_player_points, update_purchase_prices, update_selling_prices, calculate_transfer_cost, count_transfers_made
 from predictions import make_predictions, group_predictions_by_gameweek, weight_gameweek_predictions_by_availability
 from simulation.utilities import make_automatic_substitutions, get_selling_prices, get_player_name
 from simulation.loaders import load_simulation_purchase_prices, load_simulation_bootstrap_elements, load_simulation_features, load_simulation_true_results
 
 
-def get_currency_representation(amount: int):
-    """Formats game currency as a string."""
+def format_currency(amount: int):
+    """Format game currency as a string."""
     return f"${round(amount / 10, 1)}"
 
 
-def print_simulated_gameweek_report(
-        initial_squad: set, final_squad: set, 
-        selected_squad_roles: set, substituted_squad_roles: set, 
-        initial_budget: int, final_budget: int, 
-        initial_squad_selling_prices: pd.Series, 
-        final_squad_selling_prices: pd.Series,
-        final_squad_purchase_prices: pd.Series,
-        gameweek: int, 
-        player_gameweek_points: pd.Series, 
-        gameweek_points: int, 
-        total_points: int,
-        positions: pd.Series, 
-        elements: pd.DataFrame,
-        transfer_cost: int
-    ):
+def print_gameweek_report(
+    gameweek: int, 
+    initial_squad: set, 
+    final_squad: set, 
+    selected_roles: set, 
+    automatic_roles: set, 
+    final_budget: int, 
+    initial_selling_prices: pd.Series, 
+    final_purchase_prices: pd.Series,
+    player_points: pd.Series, 
+    positions: pd.Series, 
+    elements: pd.DataFrame,
+    gameweek_points: int, 
+    free_transfers: int,
+    transfer_cost: int,
+    overall_points: int,
+):
     """Print a detailed summary of activity in a simulated gameweek."""
 
     position_names = {1: 'GKP', 2: 'DEF', 3: 'MID', 4: 'FWD'}
@@ -39,63 +40,62 @@ def print_simulated_gameweek_report(
 
     # Print the starting XI, including substitutions, names, and captaincy
     print("\nStarting XI")
-    for player in sorted(substituted_squad_roles['starting_xi'], key=lambda player: positions.loc[player]):
+    for player in sorted(automatic_roles['starting_xi'], key=lambda player: positions.loc[player]):
     
-        if player not in selected_squad_roles['starting_xi']:
+        if player not in selected_roles['starting_xi']:
             print("->", end=" ")
         else:
             print("  ", end=" ")
 
-        if player == substituted_squad_roles['captain']:
+        if player == automatic_roles['captain']:
             print("(C)", end=" ")
-        elif player == substituted_squad_roles['vice_captain']:
+        elif player == automatic_roles['vice_captain']:
             print("(V)", end=" ")
         else:
             print("   ", end=" ")
 
-        print(f"{position_names[positions.loc[player]]}", end=" ")
-        print(f"{get_player_name(player, elements)}", end=" ")
-        print(f"[{sum_player_points([player], player_gameweek_points)} pts]", end=" ")
-        print()
+        print(
+            position_names[positions.loc[player]],
+            get_player_name(player, elements),
+            f"[{sum_player_points([player], player_points)} pts]"
+        )
 
-    # Print the reserve players
+    # Print the reserve players, indicating previous captains and vice-captains
     print("\nReserves")
-    for player in [substituted_squad_roles['reserve_gkp'], *substituted_squad_roles['reserve_out']]:
+    for player in [automatic_roles['reserve_gkp'], *automatic_roles['reserve_out']]:
 
-        if player in selected_squad_roles['starting_xi']:
+        if player in selected_roles['starting_xi']:
             print("<-", end=" ")
         else:
             print("  ", end=" ")
 
-        if player == selected_squad_roles['captain']:
+        if player == selected_roles['captain']:
             print("(*C)", end=" ")
-        elif player == selected_squad_roles['vice_captain']:
+        elif player == selected_roles['vice_captain']:
             print("(*V)", end=" ")
         else:
             print("    ", end=" ")
 
-        print(f"{position_names[positions.loc[player]]}", end=" ")
-        print(f"{get_player_name(player, elements)}", end=" ")
-        print(f"[{sum_player_points([player], player_gameweek_points)} pts]", end=" ")
-        print()
+        print(
+            position_names[positions.loc[player]],
+            get_player_name(player, elements),
+            f"[{sum_player_points([player], player_points)} pts]"
+        )
 
-    # Print transfer information
-    print(f"\nTransfers ({transfer_cost} points)")
+    # Print transfer activity and final budget
+    print(f"\nTransfers ({transfer_cost} points) [Free transfers: {free_transfers}]")
     for player in (final_squad - initial_squad):
-        print(f"-> {get_player_name(player, elements)} ({get_currency_representation(final_squad_purchase_prices.loc[player])})")
+        print(f"-> {get_player_name(player, elements)} ({format_currency(final_purchase_prices.loc[player])})")
     for player in (initial_squad - final_squad):
-        print(f"<- {get_player_name(player, elements)} ({get_currency_representation(initial_squad_selling_prices.loc[player])})")
+        print(f"<- {get_player_name(player, elements)} ({format_currency(initial_selling_prices.loc[player])})")
+    print(f"\nBank: {format_currency(final_budget)}")
 
-    # Print budget and squad value
-    value = final_squad_selling_prices.loc[list(final_squad)].sum()
-    print(f"\nBank: {get_currency_representation(final_budget)}")
-    print(f"Value: {get_currency_representation(value)}")
-
-    # Print total points
-    print(f"\nTotal points: {total_points}")
+    # Print overall points
+    print(f"\nOverall points: {overall_points}")
 
 
 def get_initial_team_and_budget(season: str):
+    """Return an initial squad and budget for a given season."""
 
     if season == '2021-22':
         initial_squad = {389, 237, 220, 275, 233, 359, 485, 196, 43, 177, 40, 376, 14, 62, 348}
@@ -106,63 +106,73 @@ def get_initial_team_and_budget(season: str):
     elif season == '2023-24':
         initial_squad = {352, 616, 206, 398, 209, 6, 501, 294, 303, 415, 297, 597, 368, 31, 278}
         initial_budget = 35
+    elif season == '2024-25':
+        initial_squad = {91, 455, 335, 291, 285, 328, 182, 348, 442, 401, 351, 414, 276, 475, 583}
+        initial_budget = 0
 
     return initial_squad, initial_budget
 
 
 def run_simulation(season: str, wildcard_gameweeks=[14, 25], log=False, use_cache=True) -> int:
-    """Simulate a season of FPL and return the total points scored."""
+    """Simulate a season of FPL and return the overall points scored."""
     
-    # Set up initial conditions and load results
+    # Load season results (total_points and minutes for each player in each round)
+    results = load_simulation_true_results(season, use_cache=use_cache)
+    total_points = results['total_points']
+    minutes = results['minutes']
+
+    # Initialize season statistics
     first_gameweek = 1
     last_gameweek = 38
-    initial_squad, initial_budget = get_initial_team_and_budget(season)
-    current_squad, current_budget = initial_squad, initial_budget
+    overall_points = 0
+    free_transfers = 0
+    current_squad, current_budget = get_initial_team_and_budget(season)
     purchase_prices = load_simulation_purchase_prices(season, current_squad, first_gameweek)
-    true_results = load_simulation_true_results(season, use_cache=use_cache)
-    true_total_points = true_results['total_points']
-    true_minutes = true_results['minutes']
 
 
-    total_points = 0
     for next_gameweek in range(first_gameweek, last_gameweek + 1):
 
-        # Skip GW7 in the 2022-23 season
         if (season == '2022-23') and (next_gameweek == 7):
             continue
 
-        # Load player data for the next gameweek
-        gameweek_elements = load_simulation_bootstrap_elements(season, next_gameweek)
-        now_costs = gameweek_elements['now_cost']
-        positions = gameweek_elements['element_type']
+        # Load data for the next gameweek
+        elements = load_simulation_bootstrap_elements(season, next_gameweek)
+        positions = elements['element_type']
+        now_costs = elements['now_cost']
         selling_prices = get_selling_prices(current_squad, purchase_prices, now_costs)
 
-        # Make, aggregate and process predictions
+        # Make predictions
         features = load_simulation_features(season, next_gameweek, use_cache=use_cache)
         model_path = f"models/2024-25/model-excluded-{season}.pkl"
         columns_path = f"models/2024-25/columns.json"
         predictions = make_predictions(features, model_path, columns_path)
-        gameweek_predictions = group_predictions_by_gameweek(predictions)
-        gameweek_predictions = weight_gameweek_predictions_by_availability(gameweek_predictions, gameweek_elements, next_gameweek)
-
-        if (next_gameweek == 1) or (next_gameweek in wildcard_gameweeks):
-            free_transfers = float('inf')
-            transfers_made = 0
-        else:
-            free_transfers = 1
-            transfers_made = 0
+        predictions = group_predictions_by_gameweek(predictions)
+        predictions = weight_gameweek_predictions_by_availability(predictions, elements, next_gameweek)
 
         # Optimize the squad for the next gameweek
-        best_squad = optimize_squad(
-            season, current_squad, current_budget, 
-            next_gameweek, wildcard_gameweeks,
-            now_costs, selling_prices,
-            gameweek_elements, gameweek_predictions,
-            free_transfers, transfers_made
+        solution = optimize_squad(
+            season,
+            current_squad,
+            current_budget,
+            free_transfers,
+            next_gameweek,
+            wildcard_gameweeks,
+            predictions,
+            elements,
+            now_costs,
+            selling_prices
         )
+        best_squad = solution['squad']
+        best_roles = {
+            'starting_xi': solution['starting_xi'],
+            'captain': solution['captain'],
+            'vice_captain': solution['vice_captain'],
+            'reserve_gkp': solution['reserve_gkp'],
+            'reserve_out': solution['reserve_out'],
+        }
 
-        # Update budgets and prices
-        best_squad_budget = calculate_budget(
+        # Update budget, purchase prices, and selling prices
+        updated_budget = calculate_budget(
             current_squad, best_squad, current_budget, selling_prices, now_costs
         )
         updated_purchase_prices = update_purchase_prices(
@@ -172,48 +182,59 @@ def run_simulation(season: str, wildcard_gameweeks=[14, 25], log=False, use_cach
             selling_prices, now_costs, current_squad, best_squad
         )
 
-        # Make automatic substitutions
-        best_squad_roles = suggest_squad_roles(
-            best_squad, positions, gameweek_predictions.loc[:, next_gameweek]
-        )
-        substituted_roles = make_automatic_substitutions(
-            best_squad_roles, true_minutes.loc[:, next_gameweek], positions
+        # Perform automatic substitutions
+        automatic_roles = make_automatic_substitutions(
+            best_roles, minutes.loc[:, next_gameweek], positions
         )
 
-        # Calculate points
+        # Calculate the number of points scored
         gameweek_points = calculate_points(
-            roles=substituted_roles,
-            total_points=true_total_points.loc[:, next_gameweek],
+            roles=automatic_roles,
+            total_points=total_points.loc[:, next_gameweek],
             captain_multiplier=2,
             starting_xi_multiplier=1,
             reserve_gkp_multiplier=0,
             reserve_out_multiplier=0
         )
-        total_points += gameweek_points
 
-        # Apply the cost of transfers
-        transfers_made += count_transfers_made(best_squad, current_squad)
+        # Calculate the cost of transfers
+        if next_gameweek not in {1, *wildcard_gameweeks}:
+            transfers_made = count_transfers_made(best_squad, current_squad)
+        else:
+            transfers_made = 0
+
         transfer_cost = calculate_transfer_cost(free_transfers, transfers_made)
-        total_points -= transfer_cost
 
-        # Print the gameweek report
+        # Update the overall points
+        overall_points += gameweek_points 
+        overall_points -= transfer_cost
+
         if log:
-            print_simulated_gameweek_report(
-                initial_squad=current_squad, final_squad=best_squad,
-                selected_squad_roles=best_squad_roles, substituted_squad_roles=substituted_roles,
-                initial_budget=initial_budget, final_budget=best_squad_budget,
-                initial_squad_selling_prices=selling_prices, final_squad_selling_prices=updated_selling_prices,
-                final_squad_purchase_prices=updated_purchase_prices,
-                gameweek=next_gameweek, player_gameweek_points=true_total_points.loc[:, next_gameweek], 
-                gameweek_points=gameweek_points, total_points=total_points,
-                positions=positions, elements=gameweek_elements,
-                transfer_cost=transfer_cost
+            print_gameweek_report(
+                next_gameweek,
+                current_squad,
+                best_squad,
+                best_roles,
+                automatic_roles,
+                updated_budget,
+                selling_prices,
+                updated_purchase_prices,
+                total_points.loc[:, next_gameweek],
+                positions,
+                elements,
+                gameweek_points,
+                free_transfers,
+                transfer_cost,
+                overall_points
             )
 
-        # Carry out updates
+        # Update the squad, budget, prices, and free transfers
         current_squad = best_squad
-        current_budget = best_squad_budget
+        current_budget = updated_budget
         purchase_prices = updated_purchase_prices
         selling_prices = updated_selling_prices
+        free_transfers = max(free_transfers - transfers_made + 1, 1)
+        free_transfers = min(free_transfers, 2)
 
-    return total_points
+
+    return overall_points
